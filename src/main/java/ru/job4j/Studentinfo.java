e6b532b735154080b44bf70616462a86ba23714b
AleksMulyk <108771144+AleksMulyk@users.noreply.github.com>
2022-08-10 15:06:42 -0500

    1.3. Лишние пробелы в Studentinfo [#4123]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Mulyk Aleksandr");
+        System.out.println("02.01.1986");
+    }
+}
