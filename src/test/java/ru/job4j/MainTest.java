9e0792f592944c39041e61c535ee53f751c15fe9
AleksMulyk <108771144+AleksMulyk@users.noreply.github.com>
2022-08-19 17:10:28 -0500

    5.0.1. Сумма чисел. [#226416]

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -8,7 +8,7 @@ public class MainTest {
     @Test
     public void whenActionThenResult() {
         int excepted = 1;
-        int result = 1;
-        assertThat(result, is(excepted));
+        int rsl = 1;
+        assertThat(rsl, is(excepted));
     }
 }
