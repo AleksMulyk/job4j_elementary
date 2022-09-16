1a7d24c4cc9ce434a7c69d866e0ddc0140c79f71
AleksMulyk <108771144+AleksMulyk@users.noreply.github.com>
2022-09-16 17:46:26 -0500

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        char[] codes = name.toCharArray();
+        if (name.isEmpty() || Character.isDigit(codes[0])
+                || Character.isUpperCase(codes[0]) || isSpecialSymbol(codes[0])) {
+            return false;
+        } else {
+            for (int i = 1; i < codes.length; i++) {
+                if (isSpecialSymbol(name.codePointAt(i)) || isLowerLatinLetter(name.codePointAt(i))
+                        || isUpperLatinLetter(name.codePointAt(i))
+                        || Character.isDigit(codes[i])) {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+            if (code == 36 || code == 95) {
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        if (code < 91 && code > 64) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        if (code < 123 && code > 97) {
+            return true;
+        }
+        return false;
+    }
+}
