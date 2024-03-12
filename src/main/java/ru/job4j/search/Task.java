2e3a6ea48f9284ff868897be91a891235851b963
AleksMulyk <mavpakuusa@gmail.com>
2024-03-12 14:11:55 -0500

    5. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,21 @@
+package ru.job4j.search;
+
+import javax.swing.table.TableStringConverter;
+
+public class Task {
+    private String description;
+    private int priority;
+
+    public Task(String description, int priority) {
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String  getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
